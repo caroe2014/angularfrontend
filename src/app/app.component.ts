357dadbd7539d535fd3fefd7279b1905a499f307
Edwin Caro Soto <v-edcaro@microsoft.com>
2019-10-30 13:46:58 -0500

    our commit message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angularfrontend';
+  users;
+  constructor( 
+    private appService: AppService 
+    ) {}; 
+    
+    ngOnInit() { 
+    this.users = this.appService.getUsers(); 
+    } 
 }
