2c275a26260f4a577a9325710b041533ce584020
caroe2014 <edwincaro10@hotmail.com>
2020-11-25 13:04:56 -0600

    Update app.service.ts

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -7,7 +7,7 @@ providedIn: 'root'
 export class AppService { 
 
 getUsers() { 
-return this.http.get('https://v-edcaroexpressapi.azurewebsites.net:3000/users'); 
+return this.http.get('https://v-edcaroexpressapi.azurewebsites.net/users'); 
 } 
 
 constructor( 
